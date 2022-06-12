05fa88b307a881cfb168467663ff811119a58dd7
kazukifujino <fighterkazu.0807@gmail.com>
2022-06-12 18:42:29 +0900

    作成ページ修正・更新ページ作成

--- blog.php
+++ blog.php
@@ -1,6 +1,6 @@
 <?php
-require_once('dbc.php');
 
+require_once('dbc.php');
 Class Blog extends Dbc
 {
     protected $table_name = 'blog';
@@ -25,10 +25,12 @@ Class Blog extends Dbc
     // ブログ作成
     public function blogCreate($blogs)
     {
-        $sql ='INSERT INTO 
-            blog(title,content,category,publish_status)
+        $sql =
+            "INSERT INTO 
+                blog
+                (title,content,category,publish_status)
             VALUES
-            (:title,:content,:category,:publish_status)';
+                (:title,:content,:category,:publish_status)";
 
         $dbh = $this->dbConnect();
         $dbh -> beginTransaction();
@@ -44,9 +46,69 @@ Class Blog extends Dbc
             echo '登録完了しました。';
         }catch(PDOException $e){
             $dbh->rollBack();
+            var_dump($blogs);
             exit($e);
         }
     }
-}
 
+    // ブログ更新
+    public function blogUpdate($blogs)
+    {
+        {
+            $sql =
+                "UPDATE
+                    blog
+                SET
+                    title = :title
+                    ,content = :content
+                    ,category = :category
+                    ,publish_status = :publish_status
+                WHERE
+                    id = :id";
+    
+            $dbh = $this->dbConnect();
+            $dbh -> beginTransaction();
+            try
+            {
+                $stmt = $dbh->prepare($sql);
+                $stmt ->bindValue(':title',$blogs['title'],PDO::PARAM_STR);
+                $stmt ->bindValue(':content',$blogs['content'],PDO::PARAM_STR);
+                $stmt ->bindValue(':category',$blogs['category'],PDO::PARAM_INT);
+                $stmt ->bindValue(':publish_status',$blogs['publish_status'],PDO::PARAM_INT);
+                $stmt ->bindValue(':id',$blogs['id'],PDO::PARAM_INT);
+                $stmt ->execute();
+                $dbh -> commit();
+                echo '更新完了しました。';
+            }catch(PDOException $e){
+                $dbh->rollBack();
+                exit($e);
+            }
+        }
+    }
+
+    // ブログのバリデーション
+    function blogValidate($blogs)
+    {
+        if(empty($blogs['title']))
+        {
+            exit('タイトルを入力してください');
+        }
+        if(mb_strlen($blogs['title']) > 51)
+        {
+            exit('文字数が超過しています。(50文字以内)');
+        }
+        if(empty($blogs['content']))
+        {
+            exit('本文を入力してください');
+        }
+        if(empty($blogs['category']))
+        {
+            exit('カテゴリーを選択してください');
+        }
+        if(empty($blogs['publish_status']))
+        {
+            exit('公開ステータスを選択してください');
+        }
+    }
+}
 ?>
